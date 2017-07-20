e70f66a94a98fe0b1a1e42497332fce2a89d635b
Davy <davy@Davys-MacBook-Pro.local>
2017-07-20 16:23:17 -0700

    Fixed missing openssl dependencies during mac compilation

--- install-mac.sh
+++ install-mac.sh
@@ -2,10 +2,10 @@
 brew install openssl libuv cmake zlib
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
-# git checkout e94b6e1
+git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export OPENSSL_ROOT_DIR=/usr/local/opt/openssl/
 cd build
 cmake ..
 make 
